151f9fc0cb54a20e29e9143e013a88839856cf6c
Rifat Hasan Priyo <137903375+rifathasanpriyo@users.noreply.github.com>
2024-09-07 16:27:03 +0100

    First change

---
+++ lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:body_exercise_1/drawer.dart';
+import 'package:body_exercise_1/exercise_screen/diet_cart.dart';
+import 'package:body_exercise_1/exercise_screen/exercise.dart';
+import 'package:body_exercise_1/loginpage.dart';
+import 'package:body_exercise_1/onboard/bottomnavigation.dart'; // Adjust the import as needed
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.purple,
+        scaffoldBackgroundColor: Colors.black,
+        textTheme: GoogleFonts.varelaTextTheme(),
+        bottomNavigationBarTheme: BottomNavigationBarThemeData(
+          backgroundColor: Colors.purple, // Background color of the BottomNavigationBar
+          selectedItemColor: Colors.white, // Color of the selected item
+          unselectedItemColor: Colors.grey, // Color of the unselected items
+          elevation: 8.0, // Optional: Set elevation to give shadow effect
+        ),
+      ),
+      home: 
+      LoginPage()
+      
+    );
+  }
+}
